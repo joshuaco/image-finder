18a3f7d9a1830ea1e411796265057de0cfb89c84
Joshua <joxuecont@gmail.com>
2024-01-21 09:56:53 -0400

    Fetch images & show them

---
+++ js/main.js
@@ -0,0 +1,38 @@
+const $form = document.querySelector("#search-form");
+
+$form.addEventListener("submit", getImages);
+
+async function getImages(event) {
+  event.preventDefault();
+  const $input = document.querySelector("#search");
+
+  if ($input.value === "") {
+    alert("Please enter a search term");
+    return;
+  }
+
+  const url = `https://pixabay.com/api/?key=41783426-796452a40e4a9900db65b9de1&q=${$input.value}&image_type=photo`;
+
+  const response = await fetch(url);
+  const data = await response.json();
+
+  if (data.hits.length === 0) {
+    alert("No images found");
+    return;
+  }
+
+  console.log(data);
+
+  displayImages(data.hits);
+}
+
+function displayImages(images) {
+  const $section = document.querySelector("section");
+
+  images.forEach((image) => {
+    const $img = document.createElement("img");
+    $img.src = image.webformatURL;
+    $img.alt = image.tags;
+    $section.appendChild($img);
+  });
+}
